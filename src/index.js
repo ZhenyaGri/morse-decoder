2870c66fb6b393bb8ca2dd534e9544b5848fefc4
Evgeniya <eva.gribova.2003@gmail.com>
2024-08-15 19:59:56 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -37,8 +37,38 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+
+    let ar = [];
+    flag = 0;
+    while (flag <= expr.length){
+        ar.push(expr.substring(flag, flag+10));
+        flag += 10;
+    }
+    let m = '';
+    for (str of ar){
+        if (str == '**********'){
+            m += ' ';
+        }
+        else {
+            if (str.indexOf('1') != -1){
+                let morse = str.slice(str.indexOf('1'), str.lenght);
+                let newStr = '';
+                for (let i = 0; i <= morse.length; i +=2){
+                    if (morse.substring(i, i+2) == '10' ){
+                        newStr += '.';
+                    }
+                    else if (morse.substring(i, i+2) == '11' ){
+                        newStr += '-';
+                    }
+                }
+                m += MORSE_TABLE[newStr];
+            }
+        }
+    }
+    return m
 }
 
 module.exports = {
